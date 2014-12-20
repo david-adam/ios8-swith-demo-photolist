e925368c8d809cd181bf1904acc3e1d9082266c8
David Adam <david.g.adam@gmail.com>
2014-12-20 15:35:02 +0800

    Initial Commit

---
+++ photolistTests/photolistTests.swift
@@ -0,0 +1,36 @@
+//
+//  photolistTests.swift
+//  photolistTests
+//
+//  Created by Wei Dai on 12/20/14.
+//  Copyright (c) 2014 Wei Dai. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class photolistTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
